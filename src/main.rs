1af1f90470024bcc9f0f7161ed19f76816c2f57e
ShawnNewton <shawn.newton3@gmail.com>
2025-09-27 18:34:33 -0400

    modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use pdf_extract::extract_text;
+
+use std::path::PathBuf;
+
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let pdf_location = "";
+
+    // Read the PDF file as bytes
+    let file = PathBuf::from(&pdf_location);
+
+
+    // Extract text from the PDF
+    let text = extract_text(&file)?;
+    
+    // Print the extracted text
+    println!("Extracted text:\n{}", text);
+    
+    Ok(())
 }
